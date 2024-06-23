b458db470153340e405172c446ba57db9f80f08f
Carlos Ramos <devcarlosramos@gmail.com>
2024-06-22 22:20:15 -0500

    inital commit

---
+++ src/main/java/dev/carlosramos/bs_tarjeta/BsTarjetaApplication.java
@@ -0,0 +1,13 @@
+package dev.carlosramos.bs_tarjeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsTarjetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsTarjetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/carlosramos/bs_tarjeta/BsTarjetaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.carlosramos.bs_tarjeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsTarjetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
